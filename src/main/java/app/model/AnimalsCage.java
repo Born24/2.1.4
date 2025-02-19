478ef1bfe21a863567afde8908626a9110aa0c94
Born24 <106234496+Born24@users.noreply.github.com>
2025-02-19 15:51:26 +0300

    привел инжекты к одному виду

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,17 @@
 package app.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
-    @Autowired
-    @Qualifier("dog")
     private Animal animal;
     private final Timer timer;
 
-    public AnimalsCage(Timer timer) {
+    public AnimalsCage(@Qualifier("dog")Animal animal, Timer timer) {
         this.timer = timer;
+        this.animal = animal;
     }
 
     public void whatAnimalSay() {
